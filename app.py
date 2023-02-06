f7ecb690d0adf547e6d6fe5ea69479edcec704fd
MateusBrandeburski <mateus.brandeburski92@gmail.com>
2023-02-06 13:01:45 -0300

    dsadmas

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, request, render_template
+
+
+# app = Flask(__name__, temmplate_folder='templetes')
+
+# @app.route('/', methods=['GET','POST'])
+# def index():
+#     pass
---
+++ poo.py
@@ -0,0 +1,21 @@
+
+
+class Vendedor():
+    
+    def __init__ (self, nome):
+        
+        self.nome = nome
+        self.vendas = 0
+        
+    def vendeu(self, vendas):
+        self.vendas= vendas
+        
+    def bateu_meta(self, meta):
+        if self.vendas > meta:
+            print(self.nome, "bateu a meta")
+        else:
+            print(self.nome, "não bateu a meta")
+
+vendedor1 = Vendedor("Mateus")
+vendedor1.vendeu(1000)
+vendedor1.bateu_meta(1001)
